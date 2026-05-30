d9304496607797f190fa728d191da3393e3f183c
Shivam Wadje <shivamwadje22@gmail.com>
2026-05-30 00:56:02 -0700

    Add Marks.py percentage program

---
+++ Marks.py
@@ -0,0 +1,17 @@
+A=int(input("Ente a marks A:"))
+B=int(input("Enter a marks B:"))
+C=int(input("Enter a marks C:"))
+D=int(input("Enter a marks D:"))
+E=int(input("Enter a marks E:"))
+total=A+B+C+D+E
+percentage=total/5 
+print("total marks=",total)
+print("percentage=",percentage)
+if percentage>=75:
+    print("Reslt is: distinction")
+elif percentage>=60:
+    print("Result is:First clss")
+elif percentage>=45:
+    print("Result is: pass")
+else:
+    print("Result is:Fail") 
