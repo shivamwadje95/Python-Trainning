16773540450a2b8dbca11a1f4889e1327e79307e
Shivam Wadje <shivamwadje22@gmail.com>
2026-05-31 00:47:10 -0700

    Day 1: First program

---
+++ calculator.py
@@ -0,0 +1,6 @@
+num1= int(input("Enter first number:"))
+num2= int(input("Enter second number:"))
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"product: {num1 * num2}")
+print(f"Division: {num1/num2}")
