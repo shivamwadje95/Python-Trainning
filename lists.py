1c7a3fde1c9fec73b7a0a963f85908691cc77a94
Shivam Wadje <shivamwadje22@gmail.com>
2026-05-31 01:17:55 -0700

    Day 3: Third program

---
+++ lists.py
@@ -0,0 +1,15 @@
+students = ["Rahul", "Pryan", "Satyam", "Shivam", "Karan"]
+
+print(students)
+print(students[0])
+print(students[2])
+print(len(students))
+
+students.append("Ajay")
+print(students)
+
+students.remove("Satyam")
+print(students)
+
+print("Rahul" in students)
+print("Satyam" in students)
